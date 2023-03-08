9cdec9d04ec2b50301c529dd9e9b31133be3b6b7
Hojjat musavi <106033243+Hojjat-mvi@users.noreply.github.com>
2023-03-08 20:36:30 +0330

    Update movieApi.ts

--- src/services/movieApi.ts
+++ src/services/movieApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const moviesApi = createApi({
   reducerPath: "moviesApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
   endpoints: (builder) => ({
     movies: builder.query({
       query: () => ({
